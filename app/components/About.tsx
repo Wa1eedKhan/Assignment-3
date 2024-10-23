b62eac32542e48901e540435bc566ec9576e5c5a
Waleedkhan2006 <166182415+Wa1eedKhan@users.noreply.github.com>
2024-10-23 13:25:32 +0500

    assignment-3

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,11 +7,13 @@ const About = () => {
         <h2 className="text-black text-4xl font-semibold mb-8">About</h2>
 
         <p className="text-black mb-6">
-          Hi, I’m <strong className="text-black">Waleed Khan</strong>, a passionate front-end developer with expertise in HTML, CSS, JavaScript, and TypeScript. I specialize in creating modern, responsive designs using Tailwind CSS and am continuously advancing my skills in Next.js and React to build high-performance web applications.
+        Hi, I’m <strong className="text-black">Waleed Khan</strong>, a passionate front-end developer with expertise in HTML, CSS, JavaScript, and TypeScript.
+         I specialize in creating modern, responsive designs using Tailwind CSS and am continuously advancing my skills in Next.js and React to build 
+         high-performance web applications.
         </p>
 
         <Link href="#projects">
-          <button className="px-8 py-3 bg-blue-300 text-black text-lg rounded-lg shadow-md hover:bg-blue-200 transition duration-300 transform hover:scale-105">
+      <button className="px-8 py-3 bg-blue-300 text-black text-lg rounded-lg shadow-md hover:bg-blue-200 transition duration-300 transform hover:scale-105">
             View My Work
           </button>
         </Link>
